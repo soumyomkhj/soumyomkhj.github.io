91ac12ae3f704597f8a011878e346c015a21b95f
Soumyoprabho Mukherjee <soumya.rocks26@gmail.com>
2021-07-10 01:10:06 +0530

    Made into portfolio website

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$( "#dark" ).click(function() {
+    $("html").toggleClass("invert");
+    $("img").toggleClass("invert");
+    $(".f-item").toggleClass("invert");
+    $("#dark").toggleClass("darkicon");
+  });
