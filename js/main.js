2f6994ae5e5ad0bc31ab80a6ab809b48e3aca5fb
Soumyoprabho Mukherjee <soumya.rocks26@gmail.com>
2021-07-11 02:55:21 +0530

    hover added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
+// dark mode
 $( "#dark" ).click(function() {
-    $("html").toggleClass("invert");
-    $("img").toggleClass("invert");
-    $(".f-item").toggleClass("invert");
-    $("#dark").toggleClass("darkicon");
+  $("html").toggleClass("invert");
+  $("img").toggleClass("invert");
+  $(".f-item").toggleClass("invert");
+  $("#dark").toggleClass("darkicon");
+  $(".follower").css({
+    "width": "100px",
+    "height": "100px",        
   });
+});
+
+$(".hover").hover(function () {
+    // over
+    $(".follower").css({
+      "width": "100px",
+      "height": "100px",        
+      "opacity":"100%",       
+    });
+  }, function () {
+    // out
+    $(".follower").css({
+      "width": "20px",
+      "height": "20px",
+      "opacity":"30%",       
+    });
+  }
+);
+
+$( ".hover" )
+
+// cursor
+$(document).on('mousemove', (event) => {
+  $('.follower').css({
+    left: event.clientX,
+    top: event.clientY,
+  });
+});
+
+
+// Scroll animation
 var topScr = $(this).scrollTop();
 $(".container").scroll(function () { 
   var curScr = $(this).scrollTop();
@@ -19,6 +54,10 @@ $(".container").scroll(function () {
   if (diff==1||diff==-1) {
     diff=0;
   }
-  console.log(diff);
+  //console.log(diff);
   $(".container").css('transform', 'skewY(' + diff + 'deg)');
+  diff=diff*.1;
+  $(".h1").css({
+    "text-shadow": "0px "+diff+"em 0px rgb(18, 40, 46)",
+  });
 });
