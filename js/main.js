833ab762b1d90c0b8df23ac670dac3d1b973f4a5
Soumyoprabho Mukherjee <soumya.rocks26@gmail.com>
2021-07-11 01:52:52 +0530

    Scroll Animation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ $( "#dark" ).click(function() {
     $(".f-item").toggleClass("invert");
     $("#dark").toggleClass("darkicon");
   });
+var topScr = $(this).scrollTop();
+$(".container").scroll(function () { 
+  var curScr = $(this).scrollTop();
+  var diff= topScr-curScr;
+  topScr=curScr;
+  var max=25;
+  if (diff>max) {
+    diff=max;
+  }
+  if (diff<-max) {
+    diff=-max;
+  }
+  if (diff==1||diff==-1) {
+    diff=0;
+  }
+  console.log(diff);
+  $(".container").css('transform', 'skewY(' + diff + 'deg)');
+});
